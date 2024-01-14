3571ed0b7067e83777e30c4f5b0122280d7aecaf
mohitharle <harlemohit7@gmail.com>
2024-01-14 17:23:06 +0530

    javabasics 1st lab

---
+++ src/javabasics/Identifier2.java
@@ -0,0 +1,11 @@
+package javabasics;
+//any name in java is called identifier(name could be class name,variable name,method name,etc)
+public class Identifier2 {
+	String name;
+	int age24;
+	String branch;
+	int rollNo;
+	char section;
+	String College;
+	double percentage;
+}
